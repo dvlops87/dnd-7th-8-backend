eecca81d8e45924dad6bbbe09437fe0b81edecbd
YoungDaLee <ldy0956@gmail.com>
2022-08-15 18:36:04 +0900

    Fix: drink, recipe, comment id 수정

--- DB/DDL/recipe_tb.sql
+++ DB/DDL/recipe_tb.sql
@@ -19,15 +19,15 @@ CREATE TABLE recipe(
 
 
 CREATE TABLE recipe_main_meterial(
-    `recipe_id`         INTEGER         NOT NULL    AUTO_INCREMENT    COMMENT '레시피ID',
-    `drink_id`          INTEGER         NOT NULL    AUTO_INCREMENT    COMMENT '음료ID',
+    `recipe_id`         INTEGER         NOT NULL    COMMENT '레시피ID',
+    `drink_id`          INTEGER         NOT NULL    COMMENT '음료ID',
     PRIMARY KEY (`recipe_id`, `drink_id`)
 )ENGINE=INNODB CHARSET=utf8mb4 COMMENT='레시피 메인 재료(음료) 테이블';
 
 
 CREATE TABLE recipe_sub_meterial(
-    `recipe_id`         INTEGER         NOT NULL    AUTO_INCREMENT    COMMENT '레시피ID',
-    `meterial_id`       INTEGER         NOT NULL    AUTO_INCREMENT    COMMENT '재료ID',
+    `recipe_id`         INTEGER         NOT NULL    COMMENT '레시피ID',
+    `meterial_id`       INTEGER         NOT NULL    COMMENT '재료ID',
     PRIMARY KEY (`recipe_id`, `meterial_id`)
 )ENGINE=INNODB CHARSET=utf8mb4 COMMENT='레시피 부재료 테이블';
 
@@ -41,14 +41,14 @@ CREATE TABLE recipe_meterial(
 
 
 CREATE TABLE recipe_tag(
-    `recipe_id`         INTEGER         NOT NULL    AUTO_INCREMENT    COMMENT '레시피ID',
+    `recipe_id`         INTEGER         NOT NULL    COMMENT '레시피ID',
     `tag`               VARCHAR(50)     NOT NULL    COMMENT '태그',
     PRIMARY KEY (`recipe_id`)
 )ENGINE=INNODB CHARSET=utf8mb4 COMMENT='레시피 태그 테이블';
 
 
 CREATE TABLE recipe_comment(
-    `recipe_id`         INTEGER         NOT NULL    AUTO_INCREMENT    COMMENT '레시피ID',
+    `recipe_id`         INTEGER         NOT NULL    COMMENT '레시피ID',
     `comment_id`        INTEGER         NOT NULL    AUTO_INCREMENT    COMMENT '댓글ID',
     `customer_uuid`     VARCHAR(40)     NOT NULL    COMMENT '유저고유ID',
     `comment`           VARCHAR(255)    NOT NULL    COMMENT '댓글',
@@ -59,13 +59,13 @@ CREATE TABLE recipe_comment(
 
 CREATE TABLE recipe_like(
     `customer_uuid`     VARCHAR(40)     NOT NULL    COMMENT '유저고유ID',
-    `recipe_id`         INTEGER         NOT NULL    AUTO_INCREMENT    COMMENT '레시피ID',
+    `recipe_id`         INTEGER         NOT NULL    COMMENT '레시피ID',
     PRIMARY KEY (`customer_uuid`, `recipe_id`)
 )ENGINE=INNODB CHARSET=utf8mb4 COMMENT='좋아요한 레시피 테이블';
 
 
 CREATE TABLE recipe_comment_like(
     `customer_uuid`     VARCHAR(40)     NOT NULL    COMMENT '유저고유ID',
-    `comment_id`        INTEGER         NOT NULL    AUTO_INCREMENT    COMMENT '댓글ID',
+    `comment_id`        INTEGER         NOT NULL    COMMENT '댓글ID',
     PRIMARY KEY (`customer_uuid`, `comment_id`)
 )ENGINE=INNODB CHARSET=utf8mb4 COMMENT='좋아요한 레시피 댓글 테이블';
