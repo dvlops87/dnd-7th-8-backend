e09e9a20bcb6e82db58f31b2f5e4c3ffa854334a
tjdgus <tjdgus1977@gmail.com>
2022-08-17 17:22:21 +0900

    Feat : Drink, Drink_Like

---
+++ drinking_pleasure/drink_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('get/list/',views.DrinkGetList.as_view()),
+    path('get/<int:pk>',views.DrinkDetail.as_view()),
+    path('review/<int:pk>',views.DrinkReview.as_view()),
+    path('like/<int:pk>',views.DrinkLike.as_view()),
+]
