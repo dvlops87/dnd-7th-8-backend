3b338fdfd627378822abad867fe0d5a2ee21e4ea
YoungDaLee <ldy0956@gmail.com>
2022-08-20 15:56:03 +0900

    Fix: 디코딩

--- drinking_pleasure/recipe_app/util.py
+++ drinking_pleasure/recipe_app/util.py
@@ -4,20 +4,23 @@ import base64
 def preprocessing_recipe_data(data):
     data['img'] = base64.decodebytes(data['img']).decode('latin_1')
 
-    for i in range(data['main_meterial_list']):
+    for i in range(len(data['main_meterial_list'])):
         img = data['main_meterial_list'][i]['img']
-        data['main_meterial_list'][i]['img'] = base64.decodebytes(img).decode('latin_1')
+        if img:
+            data['main_meterial_list'][i]['img'] = base64.decodebytes(img).decode('latin_1')
 
-    for i in range(data['sub_meterial_list']):
+    for i in range(len(data['sub_meterial_list'])):
         img = data['sub_meterial_list'][i]['img']
-        data['sub_meterial_list'][i]['img'] = base64.decodebytes(img).decode('latin_1')
+        if img:
+            data['sub_meterial_list'][i]['img'] = base64.decodebytes(img).decode('latin_1')
 
     return data
 
 
 def preprocessing_list_data(data):
-    for i in range(data):
-        img = data['img']
-        data[i]['img'] = base64.decodebytes(img).decode('latin_1')
+    for i in range(len(data)):
+        img = data[i]['img']
+        if img:
+            data[i]['img'] = base64.decodebytes(img).decode('latin_1')
 
     return data
